f45c7459804bca2cb15f369b8f72323df87a6fb1
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-21 10:10:25 +0700

    Update install.sh
    Signed-off-by: Nguyễn Minh Hiếu <kakathic@gmail.com>

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 # Kakathic
-sudo apt install zipalign >/dev/null
 
 Likk="$GITHUB_WORKSPACE"
-apktool () { java -jar $Likk/Tools/apktool-2.6.2-f3f199-SNAPSHOT-small.jar "$@"; }
-apksign () { java -jar $Likk/Tools/apksigner.jar sign --cert "$Likk/Tools/releasekey.x509.pem" --key "$Likk/Tools/releasekey.pk8" --out "$2" "$1"; }
+
 Taive () { curl -s -L --connect-timeout 20 "$1" -o "$2"; }
 Xem () { curl -s -G -L --connect-timeout 20 "$1"; }
 Getpro () { grep -m1 "$1=" $Likk/Custom.md | cut -d = -f2; }
