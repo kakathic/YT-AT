c3a4f367aa9e71fed51ae333c7fbf3ee1f95830f
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-25 17:12:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -95,6 +95,8 @@ zip -qr "$Likk/lib/YouTube.apk" -d $lib $xoa2
 fi
 
 [ "$ICONS" == 'true' ] && echo -n "-e custom-branding " >> $Likk/logk
+[ "$SHORTS" == 'true' ] && echo -n "-e hide-shorts-button " >> $Likk/logk
+[ "$CREATE" == 'true' ] && echo -n "-e disable-create-button " >> $Likk/logk
 
 if [ "$AMOLED" == 'true' ];then
 echo -n "-e amoled " >> $Likk/logk
