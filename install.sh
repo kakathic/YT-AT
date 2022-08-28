5e92f1fc953e7a972f7c866b8fc9425e798b07e0
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-28 07:31:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ ZHex(){ xxd -r -p "$@"; }
 
 apktoolur(){
 apktool d -r -s -f "$1" -o $Likk/Nn
-apktool b -c -f $Likk/Nn -o "$Likk/Nn.apk"
+apktool b -f $Likk/Nn -o "$Likk/Nn.apk"
 apksign "$Likk/Nn.apk" "$2"
 }
 
