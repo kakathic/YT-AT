f7201207dd1db4d9f536b109e1d5f3ccec5aa5ea
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-16 21:19:40 +0700

    Update service.sh

--- Module/common/service.sh
+++ Module/common/service.sh
@@ -24,6 +24,8 @@ if [ "$stock_path" ];then
 chcon u:object_r:apk_data_file:s0 $base_path
 mount -o bind $base_path $stock_path
 else
+umount -l "$(find /data/app/*$PK* -name 'base.apk')"
+umount -l "$(find /data/app/*/*$PK* -name 'base.apk')"
 pm install -r ${0%/*}/base.apk
 apk_path=$(pm path $PK | cut -d : -f2)
 cp -rf ${0%/*}/lib ${apk_path%/*}
