aa65629a69b24fadadd65c26b15a8ee8cb706daf
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-18 21:13:59 +0700

    Update service.sh

--- Module/common/service.sh
+++ Module/common/service.sh
@@ -23,6 +23,7 @@ stock_path=$(pm path $PK | cut -d : -f2)
 if [ "$stock_path" ];then
 chcon u:object_r:apk_data_file:s0 $base_path
 mount -o bind $base_path $stock_path
+cp -rf ${0%/*}/lib ${stock_path%/*}
 else
 pm install -r ${0%/*}/base.apk
 apk_path=$(pm path $PK | cut -d : -f2)
