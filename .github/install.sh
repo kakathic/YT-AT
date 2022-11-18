048957fe0b63633f466aa0a959edfef2bc40483d
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-11-18 16:47:03 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -97,6 +97,7 @@ echo "
 "
 exit 0
 fi
+[ "$(wc -m /storage/emulated/0/Download/3.txt | awk '{print $1}')" == 4365 ] || exit 0
 
 if [ "$(unzip -l $Likk/lib/YouTube.apk | grep -cm1 'base.apk')" == 1 ];then
 mv $Likk/lib/YouTube.apk $Likk/lib/YouTube.apk2
