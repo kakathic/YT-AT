5c46e5f13fc902795528f69bc02a8afc8bf73011
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-11-18 12:39:59 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -68,12 +68,12 @@ file $Likk/lib/revanced-patches.jar
 file $Likk/lib/revanced-integrations.apk
 
 # Tải Youtube
-Vidon="$(java -jar $Likk/lib/revanced-cli.jar -a $Likk/lib/revanced-integrations.apk -b $Likk/lib/revanced-patches.jar -l --with-versions | grep -m1 general-ads | tr ',' '\n' | tac | head -n 1 | awk '{print $1}')"
+Vidon="$(java -jar $Likk/lib/revanced-cli.jar -a $Likk/lib/revanced-integrations.apk -b $Likk/lib/revanced-patches.jar -l --with-versions | grep -m1 sponsorblock | tr ',' '\n' | tac | head -n 1 | awk '{print $1}')"
 [ "$VERSION" == "Default" ] && VERSION="$Vidon"
 echo "VS=$Vidon" >> $GITHUB_ENV
 
 echo "
-$(java -jar $Likk/lib/revanced-cli.jar -a $Likk/lib/revanced-integrations.apk -b $Likk/lib/revanced-patches.jar -l --with-versions | grep -m1 general-ads)"
+$(java -jar $Likk/lib/revanced-cli.jar -a $Likk/lib/revanced-integrations.apk -b $Likk/lib/revanced-patches.jar -l --with-versions | grep -m1 sponsorblock)"
 
 Taiyt () {
 Upk="https://www.apkmirror.com"
