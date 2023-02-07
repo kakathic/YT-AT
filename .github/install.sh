ab9827d08266ea1b075b43eab45d8358f037a899
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-07 09:59:55 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -210,9 +210,8 @@ echo "
 - Build...
 "
 if [ "$TYPE" != 'true' ];then
-#( 
-bash java -jar $Likk/lib/revanced-cli.jar -m $Likk/lib/revanced-integrations.apk -b $Likk/lib/revanced-patches.jar -a "$Likk/lib/YouTube.apk" -o "$Likk/YouT.apk" -t $Likk/tmp $(cat $Likk/logk) -e microg-support
-#sed '/WARNING: warn: removing resource/d' 123.txt
+( java -jar $Likk/lib/revanced-cli.jar -m $Likk/lib/revanced-integrations.apk -b $Likk/lib/revanced-patches.jar -a "$Likk/lib/YouTube.apk" -o "$Likk/YouT.apk" -t $Likk/tmp $(cat $Likk/logk) -e microg-support >> 123.txt 2>> 123.txt
+sed '/WARNING: warn: removing resource/d' 123.txt
 [ "$OPTIMIZATION" == 'true' ] && apktoolur "$Likk/Tav/YouTube.apk" || zipalign -f 4 "$Likk/YouT.apk" "$Likk/Tav/YouTube.apk"
 cd $Likk/Tav
 tar -cf - * | xz -9kz > $Likk/Module/common/lib.tar.xz
@@ -227,11 +226,10 @@ echo '{
 }' > $Likk/Up-$ach$amoled2.json
 fi
 
-echo > $Likk/done.txt 
-#) & cpnn
+echo > $Likk/done.txt ) & cpnn
 else
 
-( java -jar $Likk/lib/revanced-cli.jar -m $Likk/lib/revanced-integrations.apk -b $Likk/lib/revanced-patches.jar -a "$Likk/lib/YouTube.apk" -o "$Likk/YouT.apk" -t $Likk/tmp $(cat $Likk/logk)
+( java -jar $Likk/lib/revanced-cli.jar -m $Likk/lib/revanced-integrations.apk -b $Likk/lib/revanced-patches.jar -a "$Likk/lib/YouTube.apk" -o "$Likk/YouT.apk" -t $Likk/tmp $(cat $Likk/logk) >> 123.txt 2>> 123.txt
 #sed '/WARNING: warn: removing resource/d' 123.txt
 [ "$OPTIMIZATION" == 'true' ] && apktoolur "$Likk/apk/YouTube.apk" || zipalign -f 4 "$Likk/YouT.apk" "$Likk/apk/YouTube.apk"
 apksign "$Likk/apk/YouTube.apk" "$Likk/Up/YT-NoRoot-$VERSION-$ach$amoled2.apk" 
