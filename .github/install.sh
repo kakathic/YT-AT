e161824d872cd75600e8f4e4ae053339b75624b0
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-14 23:30:13 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -224,19 +224,21 @@ echo '{
 "zipUrl": "https://github.com/'$GITHUB_REPOSITORY'/releases/download/V'$Vidon'/YT-Magisk-'$VERSION'-'$ach$amoled2'.Zip",
 "changelog": "https://raw.githubusercontent.com/'$GITHUB_REPOSITORY'/Vip/Zhaglog.md"
 }' > $Likk/Up-$ach$amoled2.json
+
 echo "while true; do
 Pbyt=\"\$(curl -sLG https://github.com/kakathic/YT-AT/releases/download/Up/Up-$ach$amoled2.json | grep -m1 '\"version\"' | cut -d '\"' -f4)\"
-if [ \"\$Pbyt\" ] && [ \"\$Pbyt\" == \"\$(grep -m1 'version=' \${0%/*}/module.prop | cut -d = -f2)\" ];then
+if [ \"\$Pbyt\" ] && [ \"\$Pbyt\" != \"\$(grep -m1 'version=' \${0%/*}/module.prop | cut -d = -f2)\" ];then
 curl -sL \"https://github.com/kakathic/YT-AT/releases/download/V\$Pbyt/YT-Magisk-\$Pbyt-$ach$amoled2.Zip\" -o \${0%/*}/YouTube.zip
 magisk --install-module \${0%/*}/YouTube.zip
 cp -rf /data/adb/modules_update/YouTube /data/adb/modules
 rm -fr /data/adb/modules/YouTube /data/adb/modules/YouTube/update
 break
 else
+break
+fi
 sleep 10
 Auto=\$((\$Auto + 1))
-[ "$Auto" == 10 ] && break
-fi
+[ \"\$Auto\" == 10 ] && break
 done
 " >> $Likk/Module/common/service.sh
 fi
