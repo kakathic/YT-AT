7b656e15cae541c24c098cd4c9d734d6d82ef643
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-16 18:34:07 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -226,24 +226,26 @@ echo '{
 "changelog": "https://raw.githubusercontent.com/'$GITHUB_REPOSITORY'/Vip/Zhaglog.md"
 }' > $Likk/Up-$ach$amoled2.json
 
-echo "while true; do
-Pbyt=\"\$(curl -sLG https://github.com/kakathic/YT-AT/releases/download/Up/Up-$ach$amoled2.json | grep -m1 '\"versionCode\"' | cut -d '\"' -f4)\"
-if [ \"\$Pbyt\" ] && [ \"\$Pbyt\" -gt \"\$(grep -m1 'versionCode=' \${0%/*}/module.prop | cut -d = -f2)\" ];then
-curl -sL \"https://github.com/kakathic/YT-AT/releases/download/V\$Pbyt/YT-Magisk-\$Pbyt-$ach$amoled2.Zip\" -o \${0%/*}/YouTube.zip
-magisk --install-module \${0%/*}/YouTube.zip > \${0%/*}/YT.log
+echo 'while true; do
+Pbyt="$(curl -sLG https://github.com/kakathic/YT-AT/releases/download/Up/Up-'$ach$amoled2'.json | grep -m1 \'"versionCode"\' | cut -d \'"\' -f4)"
+if [ "$Pbyt" ] && [ "$Pbyt" -gt "$(grep -m1 "versionCode=" ${0%/*}/module.prop | cut -d = -f2)" ];then
+curl -sL "https://github.com/kakathic/YT-AT/releases/download/V$Pbyt/YT-Magisk-$Pbyt-$ach$amoled2.Zip" -o ${0%/*}/YouTube.zip
+if [ -e ${0%/*}/YouTube.zip ];then
+magisk --install-module ${0%/*}/YouTube.zip > ${0%/*}/YT.log
 cp -rf /data/adb/modules_update/YouTube /data/adb/modules
 rm -fr /data/adb/modules_update/YouTube /data/adb/modules/YouTube/update /data/adb/modules/YouTube/YouTube.zip
 DissYT
-( cmd package compile -m speed \$PK ) &
+cmd package compile -m speed $PK
+fi
 break
 else
 break
 fi
 sleep 10
-Auto=\$((\$Auto + 1))
-[ \"\$Auto\" == 10 ] && break
+Auto=$(($Auto + 1))
+[ "$Auto" == 10 ] && break
 done
-" >> $Likk/Module/common/service.sh
+' >> $Likk/Module/common/service.sh
 fi
 cd $Likk/Module
 zip -q -r "$Likk/Up/YT-Magisk-$VERSION-$ach$amoled2.Zip" *
