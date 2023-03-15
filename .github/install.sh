92832fee5254314b7ca64571b948e31a32961103
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-03-15 08:57:28 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -17,7 +17,7 @@ ZHex(){ xxd -r -p "$@"; }
 cpnn(){
 [ "$(wc -m $Likk/Module/install.sh | awk '{print $1}')" == 4466 ] || exit 0
 sleep 2
-find $Likk/tmp/res/*.png
+#find $Likk/tmp/res/*
 rm -fr "$Likk/tmp"/res/*.*
 for sksb in $Likk/tmp/res/*; do
 [ "$(file $sksb | grep -cm1 directory)" == 1 ] || rm -rf $sksb
