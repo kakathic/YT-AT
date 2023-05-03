053a05b630b5b6683671b2fb9a8a8bfc4cd54800
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-03 22:13:06 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -7,11 +7,12 @@ for kck in $Vik; do
 Vidon="$(java -jar $lib1 -a $lib3 -b $lib2 -l --with-versions | grep -m1 "$kck" | tr ' ' '\n' | sed -e "s| |\n|g" | tail -n2 | sed -e "s|\n||g")"
 [ "$Vidon" ] && break
 done
+Vidon="18.05.40"
 VER="$Vidon"
 echo "  $VER"
 echo
 
-VER="18.05.40"
+
 
 echo "- Tải YouTube apk apks..."
 # Tải YouTube apk
