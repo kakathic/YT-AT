7f40929d1a3cc7381518d3cbb3ce4a80688b013f
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-03 18:57:57 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -19,7 +19,8 @@ Taive () { curl -s -L -N -H "$User" --connect-timeout 20 "$1" -o "$2"; }
 Xem () { curl -s -G -L -N -H "$User" --connect-timeout 20 "$1"; }
 XHex(){ xxd -p "$@" | tr -d "\n" | tr -d ' '; }
 ZHex(){ xxd -r -p "$@"; }
-checkfile(){ [ -e "$1" ] && echo "- Ok $1" || ( echo "$2"; exit 1 ); }
+checkfile(){ [ -e "$1" ] && echo "- Ok ${1##*/}" || ( echo "- Lỗi không không thấy file ${1##*/}"; exit 1 ); }
+checkzip(){ [ "$(file $1 | grep -cm1 'Zip')" == 1 ] && echo "- Zip ok ${1##*/}" || ( echo "- Lỗi zip ${1##*/}"; exit 1; )
 Loading(){
 while true; do
 if [ -e "$1" ] && [ -e "$2" ];then
