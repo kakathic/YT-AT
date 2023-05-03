022cac28545895ed7482659b0b8b7e48dc34fbdc
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-03 19:13:27 +0700

    Update Tai-YT.sh

--- .github/Tai-YT.sh
+++ .github/Tai-YT.sh
@@ -4,7 +4,9 @@
 # lấy dữ liệu phiên bản mặc định
 echo
 echo "- Lấy dữ liệu phiên bản YouTube..."
-Vidon="$(java -jar $lib1 -a $lib3 -b $lib2 -l --with-versions | grep -m1 hide-create-button | tr ' ' '\n' | sed -e "s| |\n|g" | tail -n2 | sed -e "s|\n||g")"
+#Vidon="$(
+java -jar $lib1 -a $lib3 -b $lib2 -l --with-versions | grep -m1 hide-create-button | tr ' ' '\n' | sed -e "s| |\n|g" | tail -n2 | sed -e "s|\n||g"
+#)"
 VER="$Vidon"
 echo "- $VER"
 
