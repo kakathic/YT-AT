f63b5dad76ec25dd4a7391d337001ea4ce4d8944
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-03 23:27:47 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -72,7 +72,7 @@ fi
 
 # MOD YouTube 
 (
-echo '- Bắt đầu quá trình xây dựng..."
+echo "- Bắt đầu quá trình xây dựng..."
 echo
 java -Djava.io.tmpdir=tmp -jar $lib1 -b $lib2 -m $lib3 -a apk/YouTube.apk -o YT.apk \
 -t tmp --options=$HOME/.github/options.toml $Tof $Ton
