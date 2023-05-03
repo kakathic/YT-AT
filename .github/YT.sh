2f5a6dd6412f35557beb3a90f04f3d0ddfb0216d
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-04 00:50:54 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -75,7 +75,7 @@ fi
 echo "▼ Bắt đầu quá trình xây dựng..."
 echo
 java -Djava.io.tmpdir=tmp -jar $lib1 -b $lib2 -m $lib3 -a apk/YouTube.apk -o YT.apk \
--t tmp --options=$HOME/.github/options.toml $Tof $Ton
+-t tmp --options=$HOME/.github/options.toml $Tof $Ton $mgo
 ) & (
 Loading "tmp/res/values" "tmp/res/values" >/dev/null
 zip -qr apk/YouTube.apk -d res/*
