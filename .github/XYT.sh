612f566a38e1ad5638a7d7147a00ca453e80f7df
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-05 00:25:26 +0700

    Update XYT.sh

--- .github/XYT.sh
+++ .github/XYT.sh
@@ -128,14 +128,6 @@ sed '/WARNING: warn: removing resource/d' Log.txt
 
 Loading "tmp/res/values" "tmp/res/values" >/dev/null
 zip -qr apk/YouTube.apk -d res/*
-sleep 2
-
-for kvc in $(ls $HOME/.github/Language); do
-mkdir -p $HOME/tmp/res/${kvc%.*}
-sed -i "/<\/resources>/d" $HOME/tmp/res/${kvc%.*}/strings.xml
-[ -e $HOME/tmp/res/${kvc%.*} ] && cat $HOME/.github/Language/$kvc | sed -e 's|<?xml version="1.0" encoding="utf-8"?>||g' -e "/<\/resources>/d" -e "/<resources>/d" >> $HOME/tmp/res/${kvc%.*}/strings.xml || cat $HOME/.github/Language/$kvc | sed "/<\/resources>/d" >> $HOME/tmp/res/${kvc%.*}/strings.xml
-echo '</resources>' >> $HOME/tmp/res/${kvc%.*}/strings.xml
-done
 
 )
 
