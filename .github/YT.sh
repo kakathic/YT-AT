154b1d05dcf357a9c5e2b3693606c794f61ad2d9
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-04 10:10:49 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -81,7 +81,7 @@ zip -qr apk/YouTube.apk -d res/*
 
 # Chờ xây dựng xong
 Loading "YT.apk" "YT.apk" >/dev/null
-[ "$TYPE" == 'true' ] && mv YT.apk Tav/YouTube.apk || ( mv YT.apk up/YT-$ach$amoled.apk; exit; )
+[ "$TYPE" == 'true' ] && mv YT.apk Tav/YouTube.apk || ( mv YT.apk up/YT-$VER-$ach$amoled.apk; exit; )
 cd Tav
 tar -cf - * | xz -9kz > $HOME/.github/Modun/common/lib.tar.xz
 cd $HOME
@@ -106,5 +106,5 @@ echo '{
 
 # Tạo module magisk
 cd $HOME/.github/Modun
-zip -qr $HOME/Up/YT-Magisk-$ach$amoled.zip *
+zip -qr $HOME/Up/YT-Magisk-$VER-$ach$amoled.zip *
 cd $HOME
