bd66ce2bdc08f09e36215bcf753127ae332fd79d
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-04 22:33:11 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -1,3 +1,45 @@
+# load dữ liệu 
+lib1="lib/revanced-cli.jar"
+lib2="lib/revanced-patches.jar"
+lib3="lib/revanced-integrations.apk"
+
+# Tải tool sta
+pbsta(){
+Vsion1="$(Xem https://github.com/revanced/$1 | grep -om1 'revanced/'$1'/releases/tag/.*\"' | sed -e 's|dev|zzz|g' -e 's|v||g' -e 's|zzz|dev|g' -e 's|\"||g')"
+Taive "https://github.com/revanced/$1/releases/download/v${Vsion1##*/}/$1-${Vsion1##*/}$3.$2" "lib/$1.$2"; }
+
+# tải tool dev
+pbdev(){
+Vsion2="$(Xem https://github.com/revanced/$1/releases | grep -om1 'revanced/'$1'/releases/tag/.*dev*..\"' | sed -e 's|dev|zzz|g' -e 's|v||g' -e 's|zzz|dev|g' -e 's|\"||g')"
+Taive "https://github.com/revanced/$1/releases/download/v${Vsion2##*/}/$1-${Vsion2##*/}$3.$2" "lib/$1.$2"; }
+
+# tải apk
+TaiYT(){
+urrl="https://www.apkmirror.com"
+uak1="$urrl$(Xem "$urrl/apk/$2" | grep -m1 'downloadButton' | tr ' ' '\n' | grep -m1 'href=' | cut -d \" -f2)"
+uak2="$urrl$(Xem "$uak1" | grep -m1 '>here<' | tr ' ' '\n' | grep -m1 'href=' | cut -d \" -f2)"
+Taive "$uak2" "apk/$1"
+echo > "apk/$1.txt"; }
+
+# Tải tool cli
+echo "- Tải tool cli, patches, integrations..."
+if [ "$DEV" == "Develop" ];then
+echo "  Tải Dev"
+pbdev revanced-cli jar -all
+pbdev revanced-patches jar
+pbdev revanced-integrations apk
+else
+echo "  Tải Sta"
+pbsta revanced-cli jar -all
+pbsta revanced-patches jar
+pbsta revanced-integrations apk
+fi
+
+# kiểm tra tải tool
+checkzip "lib/revanced-cli.jar"
+checkzip "lib/revanced-patches.jar"
+checkzip "lib/revanced-integrations.apk"
+
 # Load dữ liệu cài đặt 
 . $HOME/.github/options/YouTube.md
 
