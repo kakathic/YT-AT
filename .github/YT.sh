0fd3e94275240a62b2acd3044c0cbd0ebd804783
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-04 09:34:55 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -8,6 +8,7 @@ Vidon="$(java -jar $lib1 -a $lib3 -b $lib2 -l --with-versions | grep -m1 "$kck" 
 [ "$Vidon" ] && break
 done
 [ "$VERSION" == 'Auto' ] && VER="$Vidon" || VER="$VERSION"
+Upenv VER "$VER"
 echo "  $VER"
 echo
 
