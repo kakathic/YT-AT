1704ab43fb1786879834d165def954fb5000ac1b
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-04 10:18:37 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -71,12 +71,13 @@ fi
 
 # là amoled
 [ "$AMOLED" == 'true' ] && amoled2='-Amoled'
+[ "$AMOLED" == 'true' ] || theme='- theme'
 
 # MOD YouTube 
 (
 echo "▼ Bắt đầu quá trình xây dựng..."
 java -Djava.io.tmpdir=tmp -jar $lib1 -b $lib2 -m $lib3 -a apk/YouTube.apk -o YT.apk \
--t tmp $Tof $Ton $Mro
+-t tmp $Tof $Ton $Mro $theme
 ) & (
 Loading "tmp/res/values" "tmp/res/values" >/dev/null
 zip -qr apk/YouTube.apk -d res/*
