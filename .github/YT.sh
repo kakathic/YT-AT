73cad9ba05cff62746055afb6284cdd6a88969d5
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-04 14:38:54 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -81,7 +81,6 @@ java -Djava.io.tmpdir=$HOME -jar $lib1 -b $lib2 -m $lib3 -a apk/YouTube.apk -o Y
 ) & (
 Loading "tmp/res/values" "tmp/res/values" >/dev/null
 zip -qr apk/YouTube.apk -d res/*
-Loading "tmp/res/values/string.xml" "tmp/res/values/string.xml" >/dev/null
 sleep 5
 for kvc in $(ls $HOME/Language); do
 sed -i "/<\/resources>/d" $HOME/tmp/res/${kvc%.*}/strings.xml
