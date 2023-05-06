8cdd06c9675c95958ccb4095d5c7f781a78f5942
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-06 13:05:01 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -1,4 +1,3 @@
-
 # load dữ liệu 
 lib1="lib/revanced-cli.jar"
 lib2="lib/revanced-patches.jar"
@@ -81,7 +80,11 @@ elif [ "$VERSION" == 'Autu' ];then
 VER="$Vidon"
 Kad=Auto
 V=U
-[ "$(Xem https://github.com/kakathic/YT-AT/releases/download/Up/Up-Z$V$ach$amoled2.json | grep -cm1 "${VER//./}")" == 1 ] && (echo; echo "- Là phiên bản mới nhất."; exit 0;)
+if [ "$(Xem https://github.com/kakathic/YT-AT/releases/download/Up/Up-Z$V$ach$amoled2.json | grep -cm1 "${VER//./}")" == 1 ];then
+echo
+echo "- Là phiên bản mới nhất."
+exit 0
+fi
 else
 VER="$VERSION"
 Kad=News
