8317a597421b2b1164895b0ed225241681d75964
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-05-21 17:41:07 +0700

    Update service.sh

--- .github/Modun/common/service.sh
+++ .github/Modun/common/service.sh
@@ -6,13 +6,13 @@ done
 MODPATH="${0%/*}"
 . $MODPATH/YT.sh
 
-if [ "$(ls -l $MODPATH/system/app/YouTube/YouTube.apk | awk '{print $5}')" == "$(cat $MODPATH/SIZE)" ];then
-mountYT "$MODPATH/YouTube.apk" "$MODPATH/system/app/YouTube/YouTube.apk"
+if [ "$(ls -l $MODPATH/base.apk | awk '{print $5}')" == "$(cat $MODPATH/SIZE)" ];then
+mountYT "$MODPATH/YouTube.apk" "$(linkAPK)"
 offCH
 else
-installYT $MODPATH/system/app/YouTube/YouTube.apk
-ls -l "$MODPATH/system/app/YouTube/YouTube.apk" | awk '{print $5}' > $MODPATH/SIZE
-cpLIB $MODPATH/system/app/YouTube/lib "$(linkAPK)"
+installYT $MODPATH/base.apk
+ls -l "$MODPATH/base.apk" | awk '{print $5}' > $MODPATH/SIZE
+cpLIB $MODPATH/lib "$(linkAPK)"
 mountYT $MODPATH/YouTube.apk "$(linkAPK)"
 offCH
 fi
