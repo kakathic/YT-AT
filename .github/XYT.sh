635237cb260572540379434c5de11a39a660bf57
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-08-15 01:20:20 +0700

    Update XYT.sh

--- .github/XYT.sh
+++ .github/XYT.sh
@@ -23,7 +23,7 @@ uak1="$urrl$(Xem "$urrl/apk/$2" | grep -m1 'downloadButton' | tr ' ' '\n' | grep
 uak2="$urrl$(Xem "$uak1" | grep -m1 '>here<' | tr ' ' '\n' | grep -m1 'href=' | cut -d \" -f2 | sed 's|amp;||')"
 Taive "$uak2" "apk/$1"
 echo "Link: $uak2"
-[ "$(file apk/$1 | grep -cm1 'Zip')" == 1 ] && echo > "apk/$1.txt"; }
+[ "$(file apk/$1 | grep -cm1 'Zip')" == 1 ] && echo > "apk/$1.txt" || ( echo "- Lỗi tải file $1"; exit 1; ); }
 
 # Tải tool cli
 echo "- Tải tool cli, patches, integrations..."
