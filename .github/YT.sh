aedb05cd1e42a14fc72e2a91c05b3b2d0bcaef69
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-08-15 01:55:18 +0700

    Update YT.sh

--- .github/YT.sh
+++ .github/YT.sh
@@ -48,9 +48,8 @@ checkzip "lib/revanced-patches.jar"
 checkzip "lib/revanced-integrations.apk"
 echo
 
-java -jar "$lib1" -a "$lib3" -b "$lib2" -l --with-versions
+java -jar "$lib1" -a "$lib3" -b "$lib2" -l --with-versions | grep -m1 'copy-video-url'
 echo
-
 
 # Load dữ liệu cài đặt 
 . $HOME/.github/options/YouTube.md
